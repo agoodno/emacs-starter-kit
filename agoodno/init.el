3de27416f7bb2b2100513fc6bd5d0cdbde135928
Andrew Goodnough <agoodno@gmail.com>
2011-09-06 14:21:22 -0500

    Integrated work machine configurations

--- agoodno/init.el
+++ agoodno/init.el
@@ -12,3 +12,9 @@
   ;; If there is more than one, they won't work right.
  '(default ((t (:inherit nil :stipple nil :background "black" :foreground "white" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 90 :width normal :foundry "unknown" :family "Liberation Mono")))))
 
+;; Start ERC config
+;;(erc :server "irc.wicourts.gov" :port 6667 :nick "agoodno")
+;;(setq erc-autojoin-channels-alist
+;;          '(("irc.wicourts.gov" "#ccap3" "#db" "#jenkins")))
+;; End ERC config
+
--- custom.el
+++ custom.el
@@ -1,27 +1,16 @@
 ;;Stop splash screen
 (setq inhibit-splash-screen t)
 
-;;Indent with spaces, never with TABs
-(setq-default indent-tabs-mode nil)
+;; sets the scratch message to empty string
+(setq initial-scratch-message nil)
 
-;;Sets basic offset
-(setq c-basic-offset 2)
+;; See http://www.delorie.com/gnu/docs/elisp-manual-21/elisp_620.html
+;; and http://www.gnu.org/software/emacs/manual/elisp.pdf
+;; disable line wrap
+(setq default-truncate-lines t)
 
-;;Indent to 2 spaces
-(setq-default tab-width 2)
-
-;;Tabs stop every 2 spaces
-(setq-default tab-stop-list (quote (2 4 6 8 10)))
-
-(autoload 'blank-mode "blank-mode" "Blank Mode." t)
-
-(defun my-espresso-mode-hook ()
-  (require 'espresso)
-  (setq espresso-indent-level 2
-        indent-tabs-mode nil
-        c-basic-offset 2))
-
-(add-hook 'espresso-mode-hook 'my-espresso-mode-hook)
+;; make side by side buffers function the same as the main window
+(setq truncate-partial-width-windows nil)
 
 ;; (global-font-lock-mode t)
 
@@ -42,18 +31,156 @@
 
 ;(global-set-key "\C-q" 'backward-kill-word)
 
+;;Indent with spaces, never with TABs
+(setq-default indent-tabs-mode nil)
+
+;;Sets basic offset
+(setq c-basic-offset 2)
+
+;;Indent to 2 spaces
+(setq-default tab-width 2)
+
+;;Tabs stop every 2 spaces
+(setq-default tab-stop-list (quote (2 4 6 8 10)))
+
+;;Permanently, force TAB to insert just one TAB (in every mode):
+(global-set-key (kbd "TAB") 'tab-to-tab-stop);
+
+(autoload 'blank-mode "blank-mode" "Blank Mode." t)
+
+;; turn off tool bar
+(tool-bar-mode 0)
+
+;; turn off scroll bar
+(scroll-bar-mode -1)
+
+;; ORG MODE
+(setq org-log-done 'time)
+(setq org-log-done 'note)
+;; END ORG MODE
+
+(setq auto-mode-alist (cons '("\\.psql$" . sql-mode) auto-mode-alist))
+
+;; Add F5 to refresh current buffer
+(global-set-key [f5] 'revert-buffer)
+
+(global-set-key [f6] 'bury-buffer)
+
+(global-set-key [f8] 'comint-previous-prompt)
+
+(global-set-key [f9] 'undo)
+
+;; Add F12 to toggle line wrap
+(global-set-key [f12] 'toggle-truncate-lines)
+
+(global-set-key (kbd "C--") 'back-window)
+
+(global-set-key (kbd "C-=") 'other-window)
+
+(defalias 'yes-or-no-p 'y-or-n-p)
+
+;;; Fix junk characters in shell-mode
+(add-hook 'shell-mode-hook
+          'ansi-color-for-comint-mode-on)
+
+;;http://www.emacswiki.org/cgi-bin/emacs-en/LoadPath
+;;     (if (fboundp 'normal-top-level-add-subdirs-to-load-path)
+;;         (let* ((my-lisp-dir "~/elisp/")
+;;               (default-directory my-lisp-dir))
+;;            (setq load-path (cons my-lisp-dir load-path))
+;;            (normal-top-level-add-subdirs-to-load-path)))
+
+;;(autoload 'tail "tail" "Tail." t)
+
+;; (add-to-list 'load-path "~/.emacs.d/rinari")
+;; (require 'rinari)
+;; (add-to-list 'auto-mode-alist '("\\.rb\\'" . ruby-mode))
+
+;; (add-to-list 'load-path "~/.emacs.d/rhtml")
+;; (require 'rhtml-mode)
+;;   (add-hook 'rhtml-mode-hook
+;;     (lambda () (rinari-launch)))
+;; (add-to-list 'auto-mode-alist '("\\.rhtml\\.erb\\'" . rhtml-mode))
+
+;;For Subversion support
+(add-to-list 'vc-handled-backends 'SVN)
+
+(setenv "PAGER" "/bin/cat")
+
+(setq split-width-threshold nil)
+
+
+(defun back-window ()
+  (interactive)
+  (other-window -1))
+
+;; scala mode hooks
+(add-hook 'scala-mode-hook 'scala-turnoff-indent-tabs-mode)
+;; end Scala Mode
+
+;; Add color to a shell running in emacs 'M-x shell'
+(autoload 'ansi-color-for-comint-mode-on "ansi-color" nil t)
+(add-hook 'shell-mode-hook 'ansi-color-for-comint-mode-on)
+
+;; Makes the prompt read-only running in emacs 'M-x shell'
+(add-hook 'shell-mode-hook
+     '(lambda () (toggle-truncate-lines 1)))
+(setq comint-prompt-read-only t)
+
+
+;; http://www.emacswiki.org/emacs/ShellMode#toc3
+(global-set-key [f7] 'alt-shell-dwim)
+
+(defun alt-shell-dwim (arg)
+  "Run an inferior shell like `shell'. If an inferior shell as its I/O
+through the current buffer, then pop the next buffer in `buffer-list'
+whose name is generated from the string \"*shell*\". When called with
+an argument, start a new inferior shell whose I/O will go to a buffer
+named after the string \"*shell*\" using `generate-new-buffer-name'."
+  (interactive "P")
+  (let* ((shell-buffer-list
+   (let (blist)
+      (dolist (buff (buffer-list) blist)
+        (when (string-match "^\\*shell\\*" (buffer-name buff))
+   (setq blist (cons buff blist))))))
+   (name (if arg
+      (generate-new-buffer-name "*shell*")
+    (car shell-buffer-list))))
+    (shell name)))
+
+(defun my-espresso-mode-hook ()
+  (require 'espresso)
+  (setq espresso-indent-level 2
+        indent-tabs-mode nil
+        c-basic-offset 2))
+
+(add-hook 'espresso-mode-hook 'my-espresso-mode-hook)
+
 ;; Scala Mode
 (add-to-list 'load-path "~/.emacs.d/custom/scala-mode")
 (require 'scala-mode-auto)
 
+(defun scala-turnoff-indent-tabs-mode ()
+  (setq indent-tabs-mode nil))
+;; end Scala Mode
+
 ;; Ensime Mode
 (add-to-list 'load-path "~/.emacs.d/custom/ensime/elisp/")
+(require 'ensime)
+
+;; Load ENSIME
+(add-to-list 'load-path "/home/agoodno/.emacs.d/ensime/elisp/")
 (require 'ensime)
 
 ;; This step causes the ensime-mode to be started whenever
 ;; scala-mode is started for a buffer. You may have to customize this step
 ;; if you're not using the standard scala mode.
 (add-hook 'scala-mode-hook 'ensime-scala-mode-hook)
+
+;; MINI HOWTO:
+;; Open .scala file. M-x ensime (once per project)
+
+;; end ENSIME
 
 ;(add-to-list 'load-path
 ;             "~/.emacs.d/custom/emacs-color-theme-solarized")
