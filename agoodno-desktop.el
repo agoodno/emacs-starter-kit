b1f2e41c2791dd0f4c394b398189f3fdf4a5362b
Andrew Goodnough <agoodno@gmail.com>
2011-09-16 07:44:12 -0500

    Turned off erc startup config for now

--- agoodno-desktop.el
+++ agoodno-desktop.el
@@ -3,22 +3,22 @@
 (add-to-list 'default-frame-alist '(width . 150))
 
 ;; Start ERC config
-(erc :server "irc.wicourts.gov" :port 6667 :nick "agoodno")
-(setq erc-autojoin-channels-alist
-          '(("irc.wicourts.gov" "#ccap3" "#db" "#jenkins")))
+;;(erc :server "irc.wicourts.gov" :port 6667 :nick "agoodno")
+;;(setq erc-autojoin-channels-alist
+;;          '(("irc.wicourts.gov" "#ccap3" "#db" "#jenkins")))
 
-(defun my-notify-erc (match-type nickuserhost message)
-  "Notify when a message is received."
-  (notify (format "%s in %s"
-                  ;; Username of sender
-                  (car (split-string nickuserhost "!"))
-                  ;; Channel
-                  (or (erc-default-target) "#unknown"))
-          ;; Remove duplicate spaces
-          (replace-regexp-in-string " +" " " message)
-          :icon "emacs-snapshot"
-          :timeout -1))
+;;(defun my-notify-erc (match-type nickuserhost message)
+;;  "Notify when a message is received."
+;;  (notify (format "%s in %s"
+;;                  ;; Username of sender
+;;                  (car (split-string nickuserhost "!"))
+;;                  ;; Channel
+;;                  (or (erc-default-target) "#unknown"))
+;;          ;; Remove duplicate spaces
+;;          (replace-regexp-in-string " +" " " message)
+;;          :icon "emacs-snapshot"
+;;          :timeout -1))
 
-(add-hook 'erc-text-matched-hook 'my-notify-erc)
+;;(add-hook 'erc-text-matched-hook 'my-notify-erc)
 
 ;; End ERC config
